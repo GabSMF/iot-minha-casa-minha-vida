688e66617082df9bd06d0b38b41dadd2260bae91
Pedro Cunha <mr.pedrocunha@gmail.com>
2025-07-09 10:43:15 -0300

    Implement opmode change CB

--- src/thermostat/WiFiComms.cpp
+++ src/thermostat/WiFiComms.cpp
@@ -26,6 +26,31 @@ bool mudouTemperaturaAC(double temp_nova) {
     loop_protocolos(&comando);
 }
 
+bool mudouModoAC(MatterThermostat::ThermostatMode_t modo_novo) {
+    acCmd::Command comando;
+    switch(modo_novo) {
+        case MatterThermostat::ThermostatMode_t::THERMOSTAT_MODE_AUTO:  // automatico
+            comando.tipo = commandType::OpMode;
+            comando.instrucao.modo_operacao = stdAc::opmode_t::kAuto;
+        case MatterThermostat::ThermostatMode_t::THERMOSTAT_MODE_COOL:  // esfriar
+            comando.tipo = commandType::OpMode;
+            comando.instrucao.modo_operacao = stdAc::opmode_t::kCool;
+        case MatterThermostat::ThermostatMode_t::THERMOSTAT_MODE_DRY:   // secar
+            comando.tipo = commandType::OpMode;
+            comando.instrucao.modo_operacao = stdAc::opmode_t::kDry;
+        case MatterThermostat::ThermostatMode_t::THERMOSTAT_MODE_HEAT:  // aquecer
+            comando.tipo = commandType::OpMode;
+            comando.instrucao.modo_operacao = stdAc::opmode_t::kHeat;
+        case MatterThermostat::ThermostatMode_t::THERMOSTAT_MODE_FAN_ONLY:  // ventilar
+            comando.tipo = commandType::OpMode;
+            comando.instrucao.modo_operacao = stdAc::opmode_t::kFan;
+        case MatterThermostat::ThermostatMode_t::THERMOSTAT_MODE_OFF:   // desligar
+            comando.tipo = commandType::Power;
+            comando.instrucao.ligar = false;
+    }
+    loop_protocolos(&comando);
+}
+
 void reconectarWiFi() {
     if (WiFi.status() != WL_CONNECTED) {
         String name, password;
